25f5181ffbb8627dd91023c4b5d94e0e37077382
skc4234 <skc4234@naver.com>
2026-08-11 16:01:17 +0900

    RestController 제작 및 application.ym수정

--- src/main/java/com/sist/web/restcontroller/RecipeRestController.java
+++ src/main/java/com/sist/web/restcontroller/RecipeRestController.java
@@ -4,6 +4,7 @@ import java.util.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +18,29 @@ import lombok.RequiredArgsConstructor;
 // Router X(화면 변경 없음)
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class RecipeRestController {
 	private final RecipeService rService;
+	
+	// VueJS로 보낼 HOME 화면
+	@GetMapping("/recipe/list_vue")
+	public ResponseEntity<Map> recipe_list_vue(
+			@RequestParam("page") int page) {
+		Map map = new HashMap();
+		try {
+			List<Recipe> list=rService.recipeListData(page);
+			int[] pages=rService.getPageData(page, 12);
+			int count=rService.recipeCount();
+			map.put("list", list);
+			map.put("pages", pages);
+			map.put("count", count);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+			//return ResponseEntity.internalServerError().build();
+		}
+		return ResponseEntity.ok(map);
+	}
 	
 	// Get+Post
 	// 반드시 비동기
@@ -58,20 +80,28 @@ public class RecipeRestController {
 		return ResponseEntity.ok(map);
 	}
 	
-	@RequestMapping("/recipe/detail_vue")
+	@GetMapping("/recipe/detail_vue")
 	public ResponseEntity<Map> recipe_detail_vue(
-			@RequestParam("no") int no) {
-		Map map = new HashMap();
+			@RequestParam("no") int no){
+		Map map=new HashMap();
 		try {
-			RecipeDetail vo=rService.recipeDetailListData(no);
+			RecipeDetail vo=new RecipeDetail();
+			vo=rService.recipeDetailListData(no);
+			List<String> mList=new ArrayList<String>();
+			List<String> iList=new ArrayList<String>();
+			String[] makes=vo.getFoodmake().split("\n");
+			for(String s:makes) {
+				StringTokenizer st=new StringTokenizer(s,"^");
+				mList.add(st.nextToken());
+				iList.add(st.nextToken());
+			}
 			map.put("vo", vo);
+			map.put("mList", mList);
+			map.put("iList", iList);
 		} catch (Exception e) {
 			e.printStackTrace();
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-			//return ResponseEntity.internalServerError().build();
+			return ResponseEntity.internalServerError().build();
 		}
 		return ResponseEntity.ok(map);
 	}
-	
-	
 }
